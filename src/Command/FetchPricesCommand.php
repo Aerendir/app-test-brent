9f2dd0196e5f10a5105714b6590a75e2f22f8efd
Aerendir <hello@aerendir.me>
2022-09-06 19:46:09 +0200

    Allow to select the data source from the console.

--- src/Command/FetchPricesCommand.php
+++ src/Command/FetchPricesCommand.php
@@ -2,14 +2,15 @@
 
 namespace App\Command;
 
-use App\Fetcher\Fetcher\DataHubFetcher;
 use App\Fetcher\FetcherRegistry;
 use App\Manager\PricesManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
@@ -20,6 +21,7 @@ use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 class FetchPricesCommand extends Command
 {
     public const NAME = 'app:fetch-prices';
+    private const OPT_SOURCE = 'source';
 
     public function __construct(
         private readonly EntityManagerInterface $entityManager,
@@ -29,13 +31,35 @@ class FetchPricesCommand extends Command
         parent::__construct();
     }
 
+    protected function configure()
+    {
+        $this
+            // I prefer Options over Arguments
+            ->addOption(self::OPT_SOURCE, 's', InputOption::VALUE_REQUIRED, 'The name of the source from which fetch the prices.');
+
+        parent::configure();
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
 
         $io->title(self::NAME);
 
-        $fetcher = $this->fetcherRegistry->find(DataHubFetcher::getName());
+        $sourceName = $input->getOption(self::OPT_SOURCE);
+
+        if (null === $sourceName) {
+            $helper = $this->getHelper('question');
+            $question = new ChoiceQuestion(
+                'Please, select the source',
+                $this->fetcherRegistry->getNamesOfAvailableFetchers(),
+            );
+
+            $question->setErrorMessage('The source is invalid');
+            $sourceName = $helper->ask($input, $output, $question);
+        }
+
+        $fetcher = $this->fetcherRegistry->findByName($sourceName);
         try {
             $prices = $fetcher->fetch();
         } catch (TransportExceptionInterface $exception) {
--- src/Fetcher/FetcherRegistry.php
+++ src/Fetcher/FetcherRegistry.php
@@ -7,7 +7,7 @@ class FetcherRegistry
 
     public function __construct(private readonly iterable $fetchers) {}
 
-    public function find(string $name):FetcherInterface
+    public function findByName(string $name):FetcherInterface
     {
         foreach ($this->fetchers as $fetcher) {
             if ($fetcher::getName() === $name) {
@@ -16,5 +16,16 @@ class FetcherRegistry
         }
 
         throw new \InvalidArgumentException(sprintf('Fetcher "%s" not found.', $name));
+    }
+
+    public function getNamesOfAvailableFetchers():array
+    {
+        static $namesOfFetchers = null;
+
+        if (false === is_array($namesOfFetchers)) {
+            $namesOfFetchers = array_map(fn(FetcherInterface $fetcher) => $fetcher::getName(), array(...$this->fetchers));
+        }
+
+        return $namesOfFetchers;
     }
 }
